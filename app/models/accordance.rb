8f0ce6d5d5ad90fb5abd339c2b84ed8334bb6d67
cedric arnous <cedric.arnous@gmail.com>
2022-11-02 11:41:19 +0100

    model accordance

---
+++ app/models/accordance.rb
@@ -0,0 +1,2 @@
+class Accordance < ApplicationRecord
+end
---
+++ test/models/accordance_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class AccordanceTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
